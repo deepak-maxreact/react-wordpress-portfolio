584e87c9b000b465112df1ad25df3b42ad7eb3ac
Desh Deepak <deepak.digifootprints@gmail.com>
2026-06-19 18:18:15 +0530

    Added toast notification and form validation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,8 @@
 import Navbar from "../components/Navbar";
 import { Country, State, City } from "country-state-city";
 import { useState } from "react";
+import Swal from "sweetalert2";
+import toast from "react-hot-toast";
 
 
 
@@ -9,6 +11,73 @@ function Contact() {
      const [selectedCountry, setSelectedCountry] = useState("");
 const [selectedState, setSelectedState] = useState("");
 const [selectedCity, setSelectedCity] = useState("");
+
+const [formData, setFormData] = useState({
+  name: "",
+  mobile: "",
+  email: "",
+  date: "",
+  country: "",
+  state: "",
+  city: "",
+  address: "",
+  message: "",
+});
+
+const handleChange = (e) => {
+  setFormData({
+    ...formData,
+    [e.target.name]: e.target.value,
+  });
+};
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await fetch(
+      "http://localhost/react-wp/wp-json/portfolio/v1/contact",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
+
+    const data = await response.json();
+
+  toast.success("Enquiry Submitted Successfully!", {
+  duration: 4000,
+});
+
+    setFormData({
+      name: "",
+      mobile: "",
+      email: "",
+      date: "",
+      country: "",
+      state: "",
+      city: "",
+      address: "",
+      message: "",
+    });
+
+    setSelectedCountry("");
+    setSelectedState("");
+    setSelectedCity("");
+
+  } catch (error) {
+    console.error(error);
+
+    Swal.fire({
+      icon: "error",
+      title: "Oops!",
+      text: "Something went wrong. Please try again.",
+    });
+  }
+};
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
@@ -28,7 +97,7 @@ const [selectedCity, setSelectedCity] = useState("");
     Let's connect and build something amazing.
   </p>
 </div>
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
 
   <div className="grid md:grid-cols-2 gap-4">
 
@@ -37,21 +106,29 @@ const [selectedCity, setSelectedCity] = useState("");
         Full Name
       </label>
       <input
-        type="text"
-        placeholder="Enter Name"
-        className="w-full border border-gray-200 rounded-xl px-4 py-3"
-      />
+  type="text"
+  name="name"
+  value={formData.name}
+  onChange={handleChange}
+  placeholder="Enter Name"
+  required
+  className="w-full border border-gray-200 rounded-xl px-4 py-3"
+/>
     </div>
 
     <div>
       <label className="block text-sm font-medium mb-2">
         Mobile Number
       </label>
-      <input
-        type="tel"
-        placeholder="Enter Mobile Number"
-        className="w-full border border-gray-200 rounded-xl px-4 py-3"
-      />
+    <input
+  type="tel"
+  name="mobile"
+  value={formData.mobile}
+  onChange={handleChange}
+  placeholder="Enter Mobile Number"
+  required
+  className="w-full border border-gray-200 rounded-xl px-4 py-3"
+/>
     </div>
 
   </div>
@@ -62,21 +139,29 @@ const [selectedCity, setSelectedCity] = useState("");
       <label className="block text-sm font-medium mb-2">
         Email Address
       </label>
-      <input
-        type="email"
-        placeholder="Enter Email"
-        className="w-full border border-gray-200 rounded-xl px-4 py-3"
-      />
+     <input
+  type="email"
+  name="email"
+  value={formData.email}
+  onChange={handleChange}
+  placeholder="Enter Email"
+  required
+  className="w-full border border-gray-200 rounded-xl px-4 py-3"
+/>
     </div>
 
     <div>
       <label className="block text-sm font-medium mb-2">
         Date
       </label>
-      <input
-        type="date"
-        className="w-full border border-gray-200 rounded-xl px-4 py-3"
-      />
+     <input
+  type="date"
+  name="date"
+  value={formData.date}
+  onChange={handleChange}
+  required
+  className="w-full border border-gray-200 rounded-xl px-4 py-3"
+/>
     </div>
 
   </div>
@@ -89,10 +174,18 @@ const [selectedCity, setSelectedCity] = useState("");
 </label>
       <select
   value={selectedCountry}
+  required
 onChange={(e) => {
   setSelectedCountry(e.target.value);
   setSelectedState("");
   setSelectedCity("");
+
+  setFormData({
+    ...formData,
+    country: e.target.value,
+    state: "",
+    city: "",
+  });
 }}
   className="w-full border border-gray-200 rounded-xl px-4 py-3"
 >
@@ -116,9 +209,15 @@ onChange={(e) => {
      <select
   value={selectedState}
   onChange={(e) => {
-    setSelectedState(e.target.value);
-    setSelectedCity("");
-  }}
+  setSelectedState(e.target.value);
+  setSelectedCity("");
+
+  setFormData({
+    ...formData,
+    state: e.target.value,
+    city: "",
+  });
+}}
   className="w-full border border-gray-200 rounded-xl px-4 py-3"
 >
   <option value="">Select State</option>
@@ -140,7 +239,14 @@ onChange={(e) => {
 </label>
     <select
   value={selectedCity}
-  onChange={(e) => setSelectedCity(e.target.value)}
+  onChange={(e) => {
+    setSelectedCity(e.target.value);
+
+    setFormData({
+      ...formData,
+      city: e.target.value,
+    });
+  }}
   className="w-full border border-gray-200 rounded-xl px-4 py-3"
 >
   <option value="">Select City</option>
@@ -165,22 +271,28 @@ onChange={(e) => {
     <label className="block text-sm font-medium mb-2">
       Address
     </label>
-    <textarea
-      rows="3"
-      placeholder="Enter Full Address"
-      className="w-full border border-gray-200 rounded-xl px-4 py-3"
-    ></textarea>
+   <textarea
+  rows="3"
+  name="address"
+  value={formData.address}
+  onChange={handleChange}
+  placeholder="Enter Full Address"
+  className="w-full border border-gray-200 rounded-xl px-4 py-3"
+/>
   </div>
 
   <div>
     <label className="block text-sm font-medium mb-2">
       Message
     </label>
-    <textarea
-      rows="4"
-      placeholder="Tell me about your project..."
-      className="w-full border border-gray-200 rounded-xl px-4 py-3"
-    ></textarea>
+   <textarea
+  rows="4"
+  name="message"
+  value={formData.message}
+  onChange={handleChange}
+  placeholder="Tell me about your project..."
+  className="w-full border border-gray-200 rounded-xl px-4 py-3"
+/>
   </div>
 
   <button
